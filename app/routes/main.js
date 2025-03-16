421c2fd1a79914608c8bb115435fc06a18892987
ImFardad <fardadgoodboy@gmail.com>
2025-03-16 16:34:51 +0000

    app dissconectet

--- app/routes/main.js
+++
@@ -1,9 +0,0 @@
-// routes/main.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.redirect('/auth/register');
-});
-
-module.exports = router;
